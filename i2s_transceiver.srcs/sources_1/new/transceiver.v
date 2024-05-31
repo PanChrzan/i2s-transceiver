20975fd6507ea3466de139f1e6f10e18f8e8122d
PanChrzan <maciej.chrzanowski98@gmail.com>
2024-05-31 13:41:57 +0200

    Input data is clocked on falling edge, output data is clocked on rising edge.

--- i2s_transceiver.srcs/sources_1/new/transceiver.v
+++ i2s_transceiver.srcs/sources_1/new/transceiver.v
@@ -49,27 +49,39 @@ module i2s_transceiver(
         .clk_divide(lr_clk)
     );
 
-reg [31:0] input_data_reg;
-reg [31:0] output_data_reg;
+reg [23:0] input_data_reg;
+reg [23:0] output_data_reg;
+reg last_lr_state;
 
 reg [4:0] counter;
 
-assign tx_i2s_data = output_data_reg[counter];
+assign tx_i2s_data = (counter<24) ? output_data_reg[counter] : 0;
+
+always @(negedge s_clk or posedge rst) begin
+    if(rst) begin
+        counter <= 0;
+        output_data_reg <= 0;
+        last_lr_state <= 0;
+    end
+    else if(!s_clk) begin
+        if(counter < 24) begin
+            counter <= counter + 1;
+            last_lr_state <= lr_clk;
+        end
+        else if(last_lr_state != lr_clk) begin
+            counter <= 0;
+            output_data_reg <= input_data_reg;
+        end
+    end
+end
 
 always @(posedge s_clk or posedge rst) begin
     if(rst) begin
-        counter <= 0;
         input_data_reg <= 0;
-        output_data_reg <= 0;
     end
     else if(s_clk) begin
-        input_data_reg[counter] <= rx_i2s_data;
-        if(counter < 31) begin
-            counter <= counter + 1;
-        end
-        else begin
-            counter <= 0;
-            output_data_reg <= input_data_reg;
+        if(counter < 24) begin
+            input_data_reg[counter] <= rx_i2s_data;
         end
     end
 end
